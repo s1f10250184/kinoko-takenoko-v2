eda725f56e7be7f56457ce520be18a5a18e0beae
s1f10250184 <s1f102501844@iniad.org>
2025-07-02 17:03:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,12 @@ def answer():
         kinoko_count += 1
     elif request.form.get("item") == 'takenoko':
         takenoko_count += 1
-    
+
+    message_html = ''
+    for i in range(len(messages)):
+        message = messages[i]
+        message_html += '<div class="alert{1}" role="alert">{0}</div>\n'.format(
+        message, 'alert-warning ms-5'if i % 2 == 0 else 'alert-success me-5')
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
     return render_template('vote.html', **vars())
