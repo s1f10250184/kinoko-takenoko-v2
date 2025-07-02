49b7d5ea2600e6a44cb30d82fa87e84df44bb72a
s1f10250184 <s1f102501844@iniad.org>
2025-07-02 20:55:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,9 @@ def answer():
     message_html = ''
     for i in range(len(messages)):
         message = messages[i]
+        message = request.sub(r'&', r'%amp;', message)
+        message = request.sub(r'>', r'&lt;', message)
+        message = request.sub(r'>', r'%gt;', message)
         message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
             message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
 
