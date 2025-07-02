bcb6fdc18f23a14218e82e0c0ee08d271515ffcf
s1f10250184 <s1f102501844@iniad.org>
2025-07-02 16:20:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,16 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    global kinoko_count, takenoko_count, messages
+    if request.form.get("item") == 'kinoko':
+        kinoko_count += 1
+    elif request.form.get("item") == 'takenoko':
+        takenoko_count += 1
+
+    messages.append(request.form.get("messages"))
+    if len(messages) >= 3:
+        messages = messages[-3:]
+     
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
     return render_template('vote.html', **vars())
